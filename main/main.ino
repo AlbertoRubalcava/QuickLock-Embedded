82180deb6b460c4cd8beaa003855bfe62d74bee3
alberto rubalcava <137430320+AlbertoRubalcava@users.noreply.github.com>
2025-12-02 00:51:14 -0800

    blue led

--- main/main.ino
+++ main/main.ino
@@ -1,29 +1,23 @@
-#include <WiFi.h>
-#include <HTTPClient.h>
 #include <Wire.h>
 #include <Adafruit_PN532.h>
 
-const char* ssid = "Wifi Name";
-const char* password = "Wifi Password";
-const char* serverURL = "serverURL";
-
 Adafruit_PN532 nfc(-1, -1);
+
+const int blueLedPin = 2;                     
+
+String authorizedUID = "4C:05:3F:06";   
 
 void setup() {
   Serial.begin(115200);
-  WiFi.begin(ssid, password);
-  Serial.print("Connecting to WiFi...");
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("\nConnected to WiFi");
+
+  pinMode(blueLedPin, OUTPUT);
+  digitalWrite(blueLedPin, LOW);              
 
   nfc.begin();
   uint32_t versiondata = nfc.getFirmwareVersion();
   if (!versiondata) {
     Serial.println("Didn't find PN532");
-    while (1);
+    while (1); 
   }
   nfc.SAMConfig();
   Serial.println("Waiting for an NFC card...");
@@ -34,36 +28,28 @@ void loop() {
   uint8_t uidLength;
 
   if (nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength, 3000)) {
+
     String uidString = "";
     for (uint8_t i = 0; i < uidLength; i++) {
       if (uid[i] < 0x10) uidString += "0";
       uidString += String(uid[i], HEX);
       if (i < uidLength - 1) uidString += ":";
     }
+    uidString.toUpperCase();
 
     Serial.print("Card UID: ");
     Serial.println(uidString);
 
-    if (WiFi.status() == WL_CONNECTED) {
-      HTTPClient http;
-      http.begin(serverURL);
-      http.addHeader("Content-Type", "application/json");
+    if (uidString == authorizedUID) {
+      Serial.println("Authorized.");
 
-      String jsonPayload = "{\"card_id\":\"" + uidString + "\"}";
-      int httpResponseCode = http.POST(jsonPayload);
-
-      if (httpResponseCode > 0) {
-        Serial.print("Server response: ");
-        Serial.println(httpResponseCode);
-      } else {
-        Serial.print("Error sending data: ");
-        Serial.println(httpResponseCode);
-      }
-
-      http.end();
+      digitalWrite(blueLedPin, HIGH);   
+      delay(2000);                     
+      digitalWrite(blueLedPin, LOW);    
+    } else {
+      Serial.println("Unauthorized card.");
     }
+
     delay(1000);
-  } else {
-    // no card detected
   }
 }
